95c9d1e7722c255303e4778d1f612b39cb9ab4bc
akhtar ali <75729680+akhtaralidfc@users.noreply.github.com>
2024-06-30 01:26:15 +0530

    Time: 119 ms (69.04%), Space: 66.2 MB (75.16%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,53 +1,26 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>> &graph, vector<vector<int>> &res, vector<bool> &vis, int start, int store) {
+        vis[start] = true;
+        for (int node : graph[start]) {
+            if (!vis[node]) {
+                res[node].push_back(store);
+                dfs(graph, res, vis, node, store);
+            }
+        }
+    }
+    
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<int>adj[n];
-		
-        vector<int>in(n,0);
-		for(auto it : edges)
-        {
-            adj[it[0]].push_back(it[1]);
-            in[it[1]]++;
+        vector<vector<int>> graph(n);
+        for (auto &i : edges) {
+            graph[i[0]].push_back(i[1]);
         }
-	
-        queue<int>q;
-        
-        for(int i=0; i<n; i++)
-        {
-            if(in[i] == 0)
-                q.push(i);
-        }
-		
-        vector<set<int>>ans(n);
-        while(!q.empty()){
-            int si = q.size();
-            while(si--)
-            {
-                int top = q.front();
-                for(int i : adj[top])
-                {
-                    in[i]--;
-                    ans[i].insert(top);
-                       if(in[i] == 0)
-                           q.push(i);
-                        for(auto j : ans[top])
-                        {
-                            ans[i].insert(j);
-                        }
-                    
-                }
-                q.pop();
-            }
-        }
-        vector<vector<int>>res(n);
-        for(int i=0; i<n; i++)
-        {
-            for(auto j : ans[i])
-            {
-                res[i].push_back(j);
-            }
+        vector<vector<int>> res(n);
+        for (int i = 0; i < n; ++i) {
+            vector<bool> visited(n);
+            visited[i] = true;
+            dfs(graph, res, visited, i, i);
         }
         return res;
-
     }
 };
