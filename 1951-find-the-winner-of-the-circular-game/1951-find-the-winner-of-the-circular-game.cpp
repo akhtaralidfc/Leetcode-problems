f73b888f99c528bf5469435f5acc5cfef09cc1a0
akhtar ali <75729680+akhtaralidfc@users.noreply.github.com>
2024-07-09 01:18:54 +0530

    Time: 2 ms (75.49%), Space: 7.2 MB (77.09%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    int helper(int n,int k){
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans=(ans+k)%i;
+        }
+        return ans;
+    }
     int findTheWinner(int n, int k) {
-        if(n==1)
-            return 1;
-        else
-            return (findTheWinner(n-1,k)+k-1)%n+1;
+        return helper(n,k)+1;  
     }
 };
