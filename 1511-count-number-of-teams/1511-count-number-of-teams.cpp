2a34d878b5428150e533d4fd04e5fd35c0e2ec3c
akhtar ali <75729680+akhtaralidfc@users.noreply.github.com>
2024-07-30 00:51:09 +0530

    Time: 33 ms (62.38%), Space: 11.7 MB (51.74%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    int numTeams(vector<int>& r) {
-        int n = r.size();
+    int numTeams(vector<int>& arr) {
+        int n = arr.size();
         int result = 0;
-        for(int i = 0 ; i < n-2 ; i++){
-            for(int j = i+1 ; j < n-1 ; j++){
-                for(int k = j+1 ; k < n ; k++){
-                    if(((r[i] < r[j]) && (r[j] < r[k])) || ((r[i] > r[j]) && (r[j] > r[k]))){
-                        result++;
-                    }
+        for(int i = 1 ; i < n-1 ; i++){
+            int leftSmall = 0, leftLarge = 0;
+            int rightSmall = 0, rightLarge = 0;
+            //left part
+            for(int j = 0 ; j < i ; j++){
+                if(arr[j] < arr[i]){
+                    leftSmall++;
+                }
+                if(arr[j] > arr[i]){
+                    leftLarge++;
                 }
             }
+            //right part
+            for(int j = i+1 ; j < n ; j++){
+                if(arr[j] < arr[i]){
+                    rightSmall++;
+                }
+                if(arr[j] > arr[i]){
+                    rightLarge++;
+                }
+            }
+            result += leftSmall * rightLarge + leftLarge * rightSmall;
         }
         return result;
     }
