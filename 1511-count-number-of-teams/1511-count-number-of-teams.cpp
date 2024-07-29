3711b066006a8221b36832b2126b090ea8e54a6a
akhtar ali <75729680+akhtaralidfc@users.noreply.github.com>
2024-07-30 01:00:26 +0530

    Time: 33 ms (62.38%), Space: 11.5 MB (59.43%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int numTeams(vector<int>& arr) {
-        int n = arr.size();
-        int result = 0;
-        for(int i = 1 ; i < n-1 ; i++){
-            int leftSmall = 0, leftLarge = 0;
-            int rightSmall = 0, rightLarge = 0;
+        int n=arr.size();
+        int res=0;
+        for(int i=1;i<n-1;i++){
+            int leftsmall=0,leftlarge=0;
+            int rightsmall=0,rightlarge=0;
             //left part
-            for(int j = 0 ; j < i ; j++){
-                if(arr[j] < arr[i]){
-                    leftSmall++;
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]){
+                    leftsmall++;
                 }
-                if(arr[j] > arr[i]){
-                    leftLarge++;
+                if(arr[j]>arr[i]){
+                    leftlarge++;
                 }
             }
             //right part
-            for(int j = i+1 ; j < n ; j++){
-                if(arr[j] < arr[i]){
-                    rightSmall++;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[i]){
+                    rightsmall++;
                 }
-                if(arr[j] > arr[i]){
-                    rightLarge++;
+                if(arr[j]>arr[i]){
+                    rightlarge++;
                 }
             }
-            result += leftSmall * rightLarge + leftLarge * rightSmall;
+            // leftsmall*rightlarge is cnt of arrays of ascending array and leftlarge*rightsmall is the cnt of arrays in descending order
+            res+=(leftsmall*rightlarge) + (leftlarge*rightsmall);
         }
-        return result;
+        return res;
     }
 };
