6e172d242bc4a991116efb1fa7dc8999141b21b7
akhtar ali <75729680+akhtaralidfc@users.noreply.github.com>
2024-08-13 23:58:53 +0530

    Time: 9 ms (21.7%), Space: 13 MB (34.6%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    void comsum(vector<int> &curr, int target, int sum, vector<int> &candidates, int curind, int n){
+        if(target==sum){
+            result.push_back(curr);
+            return;
+        }
+        else if(sum>target) return;
+        for(int i=curind;i<n;i++){
+            if(i!=curind && candidates[i]==candidates[i-1]) continue;
+            sum+=candidates[i];
+            curr.push_back(candidates[i]);
+            comsum(curr,target,sum,candidates,i+1,n);
+            sum-=candidates[i];
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> curr;
+        int n=candidates.size();
+        sort(candidates.begin(),candidates.end());
+        comsum(curr, target, 0, candidates, 0, n);
+        return result;
+    }
+};
