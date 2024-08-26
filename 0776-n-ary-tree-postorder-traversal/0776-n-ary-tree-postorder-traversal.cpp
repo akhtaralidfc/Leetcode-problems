fb03076384fc08f1df90b0aae87f0c6ad1e461a3
akhtar ali <75729680+akhtaralidfc@users.noreply.github.com>
2024-08-27 00:33:40 +0530

    Time: 18 ms (22.62%), Space: 15.3 MB (49.47%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -19,19 +19,17 @@ public:
 */
 
 class Solution {
-    void postorder(Node* root, vector<int>& postOrderTraversal){
-        if(root == NULL){
-            return;
+    void postorder(Node* root, vector<int>& res){
+        if(root==NULL) return;
+        for(int i=0;i<root->children.size();i++){
+            postorder(root->children[i], res);
         }
-        for(int i=0; i<root->children.size(); i++){
-            postorder(root->children[i], postOrderTraversal);
-        }
-        postOrderTraversal.push_back(root->val);
+        res.push_back(root->val);
     }
 public:
     vector<int> postorder(Node* root) {
-        vector<int> postOrderTraversal;
-        postorder(root, postOrderTraversal);
-        return postOrderTraversal;
+        vector<int> res;
+        postorder(root, res);
+        return res;
     }
 };
